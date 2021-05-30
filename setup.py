2a112befe6ff72fb340d0c988094d4cc286c889d
Sam Nielsen <S1031474@haileybury.vic.edu.au>
2021-05-31 08:47:38 +1000

    finished 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='depressionAnalysis',
-    version='0.1.1',    
+    version='0.1.2',    
     description='Detecting depression in social media posts',
     url='https://github.com/Sam-Nielsen-Dot/depressionAnalysis',
     author='Sam Nielsen',
