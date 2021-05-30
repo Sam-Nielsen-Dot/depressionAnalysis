93263d316362d88f9ebcc9921b8330b6a785c741
Sam-Nielsen-Dot <63500524+Sam-Nielsen-Dot@users.noreply.github.com>
2021-05-30 21:49:29 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='depressionAnalysis',
+    version='0.1.0',    
+    description='Detecting depression in social media posts',
+    url='https://github.com/Sam-Nielsen-Dot/depressionAnalysis',
+    author='Sam Nielsen',
+    author_email='lenssimane@gmail.com',
+    license='MIT',
+    packages=['depressionAnalysis'],
+    install_requires=['twint>=2.1.21',
+                    'nltk>=3.5',
+                    'requests>=2.24.0'                  
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Healthcare Industry',
+        'Environment :: Win32 (MS Windows)',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
